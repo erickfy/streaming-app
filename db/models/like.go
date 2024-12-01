73defa6c70728ff4e7d047db999508382671e1f2
Erick Andrade <aguilakrakatoa@gmail.com>
2024-12-01 12:00:00 -0500

    update constraint key

--- db/models/like.go
+++ db/models/like.go
@@ -2,6 +2,7 @@ package models
 
 type Like struct {
 	ID        uint  `gorm:"primaryKey"`
+	IsLiked   bool  `gorm:"not null"`
 	UserID    uint  `gorm:"not null"`
 	User      User  `gorm:"foreignKey:UserID"`
 	VideoID   uint  `gorm:"not null"`
